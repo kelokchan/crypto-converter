e37a396a1a20e0fa87970308dac569dc73d3dc29
Kelok Chan <kelokchan@gmail.com>
2021-12-05 11:07:39 +0800

    Copy clipboard

--- App.tsx
+++ App.tsx
@@ -1,21 +1,72 @@
+import { Ionicons } from '@expo/vector-icons';
+import {
+  DarkTheme as NavigationDarkTheme,
+  DefaultTheme as NavigationDefaultTheme,
+  NavigationContainer,
+} from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  DarkTheme as PaperDarkTheme,
+  DefaultTheme as PaperDefaultTheme,
+  Provider as PaperProvider,
+} from 'react-native-paper';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import FlashMessage from 'react-native-flash-message';
+import AppContextProvider, { useApp } from './src/context/AppContext';
+import Home from './src/Home';
 
-export default function App() {
+const Stack = createNativeStackNavigator();
+const queryClient = new QueryClient();
+
+const App = () => {
+  const app = useApp();
+  const { theme, toggleTheme, loaded } = app;
+  const isDarkTheme = theme === 'dark';
+
+  if (!loaded) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer
+      theme={isDarkTheme ? NavigationDarkTheme : NavigationDefaultTheme}>
+      <QueryClientProvider client={queryClient}>
+        <PaperProvider
+          settings={{
+            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+            // @ts-ignore
+            icon: (props) => <Ionicons {...props} />,
+          }}
+          theme={isDarkTheme ? PaperDarkTheme : PaperDefaultTheme}>
+          <StatusBar style={isDarkTheme ? 'light' : 'dark'} />
+          <Stack.Navigator>
+            <Stack.Screen
+              name="Home"
+              component={Home}
+              options={{
+                title: 'Crypto Converter',
+                headerRight: () => (
+                  <Ionicons
+                    onPress={toggleTheme}
+                    name={isDarkTheme ? 'sunny' : 'moon'}
+                    size={24}
+                    color={isDarkTheme ? 'white' : 'black'}
+                  />
+                ),
+              }}
+            />
+          </Stack.Navigator>
+        </PaperProvider>
+      </QueryClientProvider>
+      <FlashMessage position="bottom" />
+    </NavigationContainer>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default () => (
+  <AppContextProvider>
+    <App />
+  </AppContextProvider>
+);
